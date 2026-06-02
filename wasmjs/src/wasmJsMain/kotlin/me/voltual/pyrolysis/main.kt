d85eb652886511a5891d8995e791375790015440
Voltual <3196570237@qq.com>
2026-06-03 00:09:20 +0800

    update

--- wasmjs/src/wasmJsMain/kotlin/me/voltual/pyrolysis/main.kt
+++ wasmjs/src/wasmJsMain/kotlin/me/voltual/pyrolysis/main.kt
@@ -45,7 +45,7 @@ fun main() {
         val fontsLoaded = remember { mutableStateOf(false) }
 
         // 3. 在 LaunchedEffect 中仅执行纯 Kotlin 状态赋值，完美避开编译器限制
-        LaunchedEffect(fontFamily) {
+        LaunchedEffect() {
             sharedThemeFontFamily = fontFamily
             fontsLoaded.value = true
         }
@@ -74,6 +74,7 @@ fun main() {
                     Button(onClick = { count++ }) {
                         Text("Click me")
                     }
+                    Text("我不好🙃")
                 }
             }
             
